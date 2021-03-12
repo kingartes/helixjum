2b92b15741c6adb482f7cb31f6104cebce0e0339
Ростислав <kingartes1989@gmail.com>
2021-03-12 21:03:04 +0200

    Helix Jump base mechanic

---
+++ Assets/Scripts/Ball/BallTracking.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTracking : MonoBehaviour
+{
+    [SerializeField] private Vector3 _directionOffset;
+    [SerializeField] private float _length;
+
+    private Ball _ball;
+    private Cylinder _cylinder;
+    private Vector3 _cameraPosition;
+    private Vector3 _minimumBallPostion;
+
+
+    private void Start()
+    {
+        _ball = FindObjectOfType<Ball>();
+        _cylinder = FindObjectOfType<Cylinder>();
+
+        _cameraPosition = _ball.transform.position;
+        _minimumBallPostion = _ball.transform.position;
+
+        TrackBall();
+    }
+
+    private void Update()
+    {
+        if(_ball.transform.position.y < _minimumBallPostion.y)
+        {
+            TrackBall();
+            _minimumBallPostion = _ball.transform.position;
+        }
+    }
+    private void TrackBall()
+    {
+        var cylinderPosition = _cylinder.transform.position;
+        cylinderPosition.y = _ball.transform.position.y;
+        _cameraPosition = _ball.transform.position;
+        var direction = (cylinderPosition - _ball.transform.position).normalized + _directionOffset;
+        _cameraPosition -= direction * _length;
+        transform.position = _cameraPosition;
+        transform.LookAt(_ball.transform);
+    }
+}
---
+++ Assets/Scripts/Tower/TowerRotator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TowerRotator : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _keyboardDelta;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            HandleTouch(touch);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            float torque = _keyboardDelta * Time.deltaTime * _rotateSpeed;
+            Rotate(torque);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            float torque = _keyboardDelta * Time.deltaTime * _rotateSpeed;
+            Rotate(-torque);
+        }
+
+    }
+
+    private void HandleTouch(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Moved)
+        {
+            float torque = touch.deltaPosition.x * Time.deltaTime * _rotateSpeed;
+            Rotate(torque);
+        }
+    }
+
+    private void Rotate(float torque)
+    {
+        _rigidbody.AddTorque(Vector3.up * torque);
+    }
+}
